dfebe2a0a65bce3b99f9466f1d2649e7a5f8653b
Jessica Irakoze <jessicairakoze4@gmail.com>
2025-09-10 16:43:00 +0200

    updated exercise

--- Exercise5.js
+++ Exercise5.js
@@ -1,8 +1,8 @@
-function Factorial(n) {
-    if (n === 0 || n === 1) return 1;
-    let factorial = 1;
-    for (let i = 2; i <= n; i++) {
-        factorial *= i;
-    }
-    return factorial;
+function factorial(n) {
+  if (n === 0 || n === 1) return 1;
+  let fact = 1;
+  for (let i = 2; i <= n; i++) {
+    fact *= i;
+  }
+  return factorial;
 }
