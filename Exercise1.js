4f20168e5aecc91919173acb493600ff46d74f7c
Jessica Irakoze <jessicairakoze4@gmail.com>
2025-09-13 23:18:12 +0200

    refactored ex2.1

--- Exercise1.js
+++ Exercise1.js
@@ -1,10 +1,17 @@
+// function calculateAverage(numbers) {
+//   if (numbers.length === 0) {
+//     return 0;
+//   }
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+//   }
+//   return sum / numbers.length;
+// }
 function calculateAverage(numbers) {
-  if (numbers.length === 0) {
-    return 0;
-  }
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i];
-  }
-  return sum / numbers.length;
+  if (numbers.length === 0) return 0;
+  return numbers.reduce((sum, currentValue) => sum + currentValue, 0) / numbers.length;
 }
+
+const numbers = [1, 2, 3, 4];
+console.log(calculateAverage(numbers)); // 2.5
