8cff6e11e7d17af9cd53e3ae340a3b29c121d420
Jessica Irakoze <jessicairakoze4@gmail.com>
2025-09-14 22:35:18 +0200

    Exercise 1

---
+++ Exercise 4/exercise1.js
@@ -0,0 +1,18 @@
+function numberFormat(n) {
+  let str = Math.abs(n).toString();
+  let result = "";
+  let count = 0;
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    result = str[i] + result;
+    count++;
+    if (count % 3 === 0 && i !== 0) {
+      result = "," + result;
+    }
+  }
+  if (n < 0) {
+    result = "-" + result;
+  }
+  return result;
+}
+
